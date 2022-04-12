c38bb6f52a7148dae6693bc458483949d51775ce
Deyan Stamboliev <93904336+stambolievv@users.noreply.github.com>
2022-04-12 17:30:45 +0300

    fix: clean up

--- pixi-5/src/api/api.js
+++
@@ -1,8 +0,0 @@
-export async function request(url) {
-  try {
-    const response = await fetch(url);
-    return response.status == 204 ? response : response.json();
-  } catch (error) {
-    throw new Error(error.message);
-  }
-}
